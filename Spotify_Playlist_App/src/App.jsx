b9f89153b1a4b67932d5abad324d73cef1772dfa
Jonathan Gian <153126165+JonathanGian@users.noreply.github.com>
2024-11-04 18:36:31 +0200

    Add basic react app to build on

---
+++ Spotify_Playlist_App/src/App.jsx
@@ -0,0 +1,15 @@
+import { Welcome } from './components/welcome/Welcome'
+import './App.css'
+import "./components/welcome/Welcome.css"
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Welcome/>
+      {/* Add other components here */}
+    </div>
+  );
+}
+
+export default App
---
+++ Spotify_Playlist_App/src/components/welcome/Welcome.jsx
@@ -0,0 +1,8 @@
+export function Welcome() {
+    return (
+        <div id="welcome">
+            <h1>Spotify Playlist App</h1>
+            <p>This is just a starting point for our project.</p>
+        </div>
+    )
+}
