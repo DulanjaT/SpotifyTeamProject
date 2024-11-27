148a4e2cf553c060cfd8e4c6e5782db405846f10
Sova-MadHouse <81741098+Sova-MadHouse@users.noreply.github.com>
2024-11-27 20:00:31 +0200

    Document requestWrapper

--- Spotify_Playlist_App/src/spotify/requestWrapper.js
+++ Spotify_Playlist_App/src/spotify/requestWrapper.js
@@ -1,3 +1,15 @@
+/**
+ * Spotify API wrapper utility for using a pair of React useState setters as callbacks
+ * 
+ * @param {string} path Api endpoint such as "me" or "users/{user_id}/playlists". May not be prefixed with a forward-slash
+ * @param {?Object} req null or Fetch RequestInit object https://developer.mozilla.org/en-US/docs/Web/API/RequestInit
+ * @param {Function} setData React useState setter function.
+ * On success, it receives the JSON response body
+ * @param {Function} setError React useState setter function.
+ * On failure, it receives an object with guaranteed property "status" containing the HTTP error code
+ * and optional property "message" if one was provided by the API.
+ * @returns
+ */
 export default async function requestWrapper(path, req, setData, setError)
 {
 	if (!req)
